d8acd07c40edba4bc2675fea9a8bf76cb39a9e7d
Mutahar Aamir <54906226+mutaharaamir@users.noreply.github.com>
2026-06-14 14:29:43 +0200

    Make init_mask layer-wise and update logging

--- main.py
+++ main.py
@@ -1,67 +1,39 @@
 import time
+import gc
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from huggingface_hub import login
+import wandb
 
 from utils import *
 
 login(token='')
 
-nsamples = 128
-sparsity = 0.5
-seed = 42
-seqlen = 2048
-drop_fraction = 0.5 # the paper suggests 0.1/0.3/0.5
-num_epochs = 5   # how many times we see the whole batch
-update_schedule = 64
-model_name = "meta-llama/Llama-3.2-1B"
-
-wandb.init(
-    project="rigl-llm",
-    name="8b-50%-0.3-64-5",   # optional, auto-generated if omitted
-    config={
-        "sparsity_ratio": sparsity,
-        "seed": seed,
-        "seqlen": seqlen,
-        "nsamples": nsamples,
-        "drop_fraction": drop_fraction,
-        "update_schedule": "every 64 samples",
-        "num_epochs": num_epochs,
-        "model": model_name,
-    }
-)
-
-wandb.run.notes = f"Sparsity: {sparsity} | Drop Frac: {drop_fraction} | Schedule: {update_schedule} | Epochs: {num_epochs} | model: {model_name}"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype=torch.float16, # loads in 32-bit on cpu otherwise
-    device_map="cpu"
-)
-
 # todo: benchmark this against magnitude pruning to make sure it's the same
 def init_mask(model, sparsity=0.5, device='cuda', seed=67):
     torch.manual_seed(seed)
-    model.to('cuda')
-    for name, module in model.named_modules():
-        if isinstance(module, nn.Linear):
-            if "lm_head" in name:
-                continue
+    layers = model.model.layers
+    for layer in layers:
+        layer.to(device)
 
-            W = module.weight.data
+        for name, module in layer.named_modules():
+            if isinstance(module, nn.Linear):
+                if "lm_head" in name:
+                    continue
 
-            rand_scores = torch.rand_like(W)
-            k = int(rand_scores.numel() * sparsity)
+                W = module.weight.data
 
-            threshold = torch.kthvalue(rand_scores.flatten(), k).values
-            mask = rand_scores > threshold
+                rand_scores = torch.rand_like(W)
+                k = int(rand_scores.numel() * sparsity)
 
-            module.register_buffer('mask', mask)
-    model.to('cpu')
+                threshold = torch.kthvalue(rand_scores.flatten(), k).values
+                mask = (rand_scores > threshold).bool()
 
-def update_mask(layer, drop_fraction=0.3):
+                module.register_buffer('mask', mask)
+        layer.to('cpu')
+        torch.cuda.empty_cache()
+
+def update_mask(layer, drop_fraction, mask_update_idx):
     for name, module in layer.named_modules():
         if not isinstance(module, nn.Linear):
             continue
@@ -91,6 +63,11 @@ def update_mask(layer, drop_fraction=0.3):
         actual_sparsity = zero_elements / total_elements
         print(f'sparsity in {name}: {actual_sparsity}')
 
+        # logging
+        wandb.log({
+            'mask_update_step': mask_update_idx,
+            f'layer_sparsity/{name}': actual_sparsity,
+        })
 
 def prune_rigl(model, tokenizer, device='cuda', sparsity=0.5, nsamples=128,
                seqlen=2048, seed=42, num_epochs=3, drop_fraction=0.3, update_schedule=64):
@@ -101,16 +78,9 @@ def prune_rigl(model, tokenizer, device='cuda', sparsity=0.5, nsamples=128,
     model.seqlen = seqlen
 
     # update schedule in the rigl paper is once every 100 batches, i could try 64 or 75
-
     start = time.perf_counter()
-    print("Starting...")
-
-    # TODO: initialize with wanda
-    init_mask(model, sparsity, seed)
-    # TODO: maybe evalute perplexity here as well to see how much it improves? but probably unnecessary sincre we have benchmarks
-    # but worth doing once as a sanity check
-
-    print(f'1. Mask Initialized. {time.perf_counter() - start:.2f}s')
+    # print(f'Mask Initialized. {time.perf_counter() - start:.2f}s')
+    print("Mask Initialized. Starting...")
 
     # 1. load calibration data
     print(f'2. Loading calibration data.')
@@ -138,6 +108,7 @@ def prune_rigl(model, tokenizer, device='cuda', sparsity=0.5, nsamples=128,
         attention_mask = attention_mask.to(device)
 
     current_inps = inps
+    mask_update_idx = 0 # todo: remove maybe?
     for layer_idx, layer in enumerate(layers):
         layer = layer.to(device)
         layer.zero_grad()
@@ -186,6 +157,7 @@ def prune_rigl(model, tokenizer, device='cuda', sparsity=0.5, nsamples=128,
             print(f'5.3 getting outputs from sparse layer and calculating reconstruction loss. {time.perf_counter() - start:.2f}s')
             running_loss = 0
             criterion = nn.MSELoss()
+            # TODO: batch these? doesnt have to be a for-loop if enough vram tbh
             for idx, inp in enumerate(current_inps):
                 inputs = inp.unsqueeze(0).to(device) # add batch dim? .unsqueeze(0)
 
@@ -207,18 +179,28 @@ def prune_rigl(model, tokenizer, device='cuda', sparsity=0.5, nsamples=128,
                 # update mask
                 if (idx+1) % update_schedule == 0:
                     print('updating mask')
-                    update_mask(layer, drop_fraction=drop_fraction)
+                    mask_update_idx += 1
+                    update_mask(layer, drop_fraction=drop_fraction, mask_update_idx=mask_update_idx) # todo: pass in layer number?
 
                 del sparse_output, dense_target, loss, inputs
                 torch.cuda.empty_cache()
 
+            # compute statistics
             epoch_loss = running_loss / len(current_inps)
             print(f'avg loss for epoch#{i+1}: {epoch_loss}')
+            global_epoch = (layer_idx * num_epochs) + (i + 1)
+
+            # logging
             wandb.log({
-                f"layer_{layer_idx+1}/loss": epoch_loss,
-                "epoch_step": i + 1
+                'epoch_step': i+1,
+                'global_epoch': global_epoch,
+                'layer_index': layer_idx + 1,
+
+                'sequential_loss': epoch_loss,
+
+                f'layer_comparison/loss_layer_{layer_idx+1}': epoch_loss,
             })
-            
+
             if i == 0:
                 initial_layer_loss = epoch_loss
 
@@ -233,36 +215,81 @@ def prune_rigl(model, tokenizer, device='cuda', sparsity=0.5, nsamples=128,
                 updated_outs.append(out)
             current_inps = torch.cat(updated_outs, dim=0)
 
-        ## removing layer from gpu
+        # removing layer from gpu
         layer = layer.to('cpu')
         print(f'Layer {layer_idx} done')
 
         final_layer_loss = epoch_loss
         delta_loss = initial_layer_loss - final_layer_loss
-        wandb.log({
-            "summary/layer_index": layer_idx + 1,
-            "summary/initial_loss": initial_layer_loss,
-            "summary/final_loss": final_layer_loss,
-            "summary/delta_improvement": delta_loss
-        })
+        wandb.log({ #todo: fix
+            f"layer_summaries/initial_loss": initial_layer_loss,
+            f"layer_summaries/final_loss": final_layer_loss,
+            f"layer_summaries/delta_improvement": delta_loss,
+            "layer_summaries/layer_index": layer_idx + 1,
+        }, step=layer_idx + 1)
 
+
+    # (re)apply all masks
+    for name, module in model.named_modules():
+        if isinstance(module, nn.Linear) and hasattr(module, 'mask'):
+            module.weight.data.mul_(module.mask)
     print("pruning finished.")
 
 
-prune_rigl(model, tokenizer, 'cuda', sparsity, nsamples, seqlen,
-           seed, num_epochs, drop_fraction, update_schedule)
+def train():
+    run = wandb.init(group="update-schedule-sweep")
+
+    nsamples = 128
+    sparsity = 0.5
+    seed = 42
+    seqlen = 2048
+    update_schedule = wandb.config.update_schedule
+    num_epochs = wandb.config.num_epochs  # how many times we see the whole batch
+    drop_fraction = wandb.config.drop_fraction # the paper suggests 0.1/0.3/0.5
+    model_name = "meta-llama/Llama-3.2-1B"
+
+    run.name = f"{model_name[-2:]}-{drop_fraction}-{num_epochs}-{sparsity*100}%-{update_schedule}"
+    run.notes = f"Sparsity: {sparsity} | Drop Frac: {drop_fraction} | Schedule: {update_schedule} | Epochs: {num_epochs} | model: {model_name}"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.float32,
+        device_map="cpu"
+    )
+
+    # initializing mask
+    init_mask(model=model, sparsity=sparsity, seed=seed)
+
+    # 3: RigL Pruning
+    prune_rigl(model, tokenizer, 'cuda', sparsity, nsamples, seqlen,
+               seed, num_epochs, drop_fraction, update_schedule)
 
 
-# (re)apply all masks
-for name, module in model.named_modules():
-    if isinstance(module, nn.Linear) and hasattr(module, 'mask'):
-        module.weight.data.mul_(module.mask)
+    # 4: calculating final perplexity
+    print("Evaluating final RigL pruned perplexity...")
+    model.to('cuda')
+    final_ppl = eval_ppl(seed, model, tokenizer)
+    print(f"Final Optimized PPL: {final_ppl}")
+    wandb.log({"ppl/3_final_rigl_pruned": final_ppl})
 
-# evaluate
-model.to('cuda')
-ppl_after = eval_ppl(42, model, tokenizer)
-print(ppl_after)
-wandb.log({"ppl/perplexity after pruning": ppl_after})
+    del model
+    gc.collect()
+    torch.cuda.empty_cache()
 
-wandb.finish()
+    run.finish()
 
+# 2. Define the configuration
+sweep_configuration = {
+    'method': 'grid',
+    'metric': {'goal': 'minimize', 'name': 'ppl/perplexity after pruning'},
+    'parameters': {
+        'drop_fraction': {'values': [0.3, 0.5]},
+        'num_epochs':{'values': [10, 15]},
+        'update_schedule': {'values': [32, 64, 128]}
+    }
+}
+
+# 3. Initialize and run everything immediately
+sweep_id = wandb.sweep(sweep=sweep_configuration, project="rigl-llm")
+wandb.agent(sweep_id, function=train)
